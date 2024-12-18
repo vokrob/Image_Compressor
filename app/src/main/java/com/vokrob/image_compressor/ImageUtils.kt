cea9770d614167f160b5706cdc477977d1d0ab26
vokrob <106639488+vokrob@users.noreply.github.com>
2024-12-18 23:47:56 +0700

    Updating the interface with design elements, adding Snackbar

--- app/src/main/java/com/vokrob/image_compressor/ImageUtils.kt
+++ app/src/main/java/com/vokrob/image_compressor/ImageUtils.kt
@@ -13,25 +13,28 @@ import java.io.FileOutputStream
 fun compressImage(context: Context, uri: Uri) {
     val inputStream = context.contentResolver.openInputStream(uri)
     val originalBitmap = BitmapFactory.decodeStream(inputStream)
-    val compressedBitmap = File(context.cacheDir, "compressed_image.jpg")
-    val outputStream = FileOutputStream(compressedBitmap)
+    val compressedFile = File(context.cacheDir, "compressed_image.jpg")
+    val outputStream = FileOutputStream(compressedFile)
 
     originalBitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
     outputStream.flush()
     outputStream.close()
 
-    saveImage(context, compressedBitmap)
+    saveImage(context, compressedFile)
 }
 
 private fun saveImage(context: Context, imageFile: File) {
     val values = ContentValues().apply {
-        put(MediaStore.Images.Media.DISPLAY_NAME, imageFile.name)
-        put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-        put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+        put(MediaStore.Images.Media.DISPLAY_NAME, imageFile.name) // Имя файла
+        put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg") // MIME-тип
+        put(
+            MediaStore.Images.Media.RELATIVE_PATH,
+            Environment.DIRECTORY_PICTURES
+        )
     }
+
     val uri: Uri? =
         context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-
     if (uri != null) {
         context.contentResolver.openOutputStream(uri)?.use { outputStream ->
             imageFile.inputStream().use { inputStream ->
@@ -40,3 +43,23 @@ private fun saveImage(context: Context, imageFile: File) {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/vokrob/image_compressor/MainContent.kt
+++ app/src/main/java/com/vokrob/image_compressor/MainContent.kt
@@ -3,23 +3,37 @@ package com.vokrob.image_compressor
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Preview(showBackground = true)
 
@@ -31,22 +45,75 @@ fun MainContent() {
         rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
             bitmapResult = uri
         }
+    val snackBarHostState = remember { SnackbarHostState() }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { launcher.launch("image/*") }) {
-            Text(text = "Выбрать изображение")
+        Text(
+            text = "Image Compressor",
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { launcher.launch("image/*") },
+            shape = RoundedCornerShape(12.dp)
+        ) {
+            Text("Выбрать изображение")
         }
         Spacer(modifier = Modifier.height(16.dp))
 
         bitmapResult?.let { uri ->
-            AsyncImage(model = uri, contentDescription = null)
-            Button(onClick = { compressImage(context, uri) }) {
-                Text(text = "Сжать и сохранить изображение")
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                shape = RoundedCornerShape(12.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    AsyncImage(model = uri, contentDescription = null)
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Button(
+                        onClick = {
+                            CoroutineScope(Dispatchers.Main).launch {
+                                compressImage(context, uri)
+                                snackBarHostState.showSnackbar("Изображение сохранено")
+                            }
+                        },
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(12.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary)
+                    ) {
+                        Text(
+                            text = "Сжать изображение и сохранить",
+                            color = Color.White
+                        )
+                    }
+                }
             }
         }
+        SnackbarHost(hostState = snackBarHostState)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
